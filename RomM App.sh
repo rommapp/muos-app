32d5e671c7bb19f1419a5177f7f91447c5ee4a53
Georges-Antoine Assi <contact@me.gantoine.com>
2025-06-01 20:52:46 -0400

    fix check errors in appsh

--- RomM App.sh
+++ RomM App.sh
@@ -2,11 +2,11 @@
 
 XDG_DATA_HOME=${XDG_DATA_HOME:-${HOME}/.local/share}
 
-if [ -d "/opt/system/Tools/PortMaster/" ]; then
+if [[ -d "/opt/system/Tools/PortMaster/" ]]; then
 	controlfolder="/opt/system/Tools/PortMaster"
-elif [ -d "/opt/tools/PortMaster/" ]; then
+elif [[ -d "/opt/tools/PortMaster/" ]]; then
 	controlfolder="/opt/tools/PortMaster"
-elif [ -d "${XDG_DATA_HOME}/PortMaster/" ]; then
+elif [[ -d "${XDG_DATA_HOME}/PortMaster/" ]]; then
 	controlfolder="${XDG_DATA_HOME}/PortMaster"
 else
 	controlfolder="/roms/ports/PortMaster"
@@ -34,34 +34,34 @@ export LD_LIBRARY_PATH="${GAMEDIR}/libs:${LD_LIBRARY_PATH}"
 export SDL_GAMECONTROLLERCONFIG="${sdl_controllerconfig}"
 
 use_runtime() {
-	if [ ! -f "$controlfolder/libs/${runtime}.squashfs" ]; then
-		if [ ! -f "$controlfolder/harbourmaster" ]; then
+	if [[ ! -f "${controlfolder}/libs/${runtime}.squashfs" ]]; then
+		if [[ ! -f "${controlfolder}/harbourmaster" ]]; then
 			pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/"
 			sleep 5
 			exit 1
 		fi
-		$ESUDO "$controlfolder/harbourmaster" --quiet --no-check runtime_check "${runtime}.squashfs"
+		${ESUDO} "${controlfolder}/harbourmaster" --quiet --no-check runtime_check "${runtime}.squashfs"
 	fi
 
-	$ESUDO mkdir -p "${python_dir}"
-	$ESUDO umount "$controlfolder/libs/${runtime}.squashfs" 2>/dev/null || true
-	$ESUDO mount "$controlfolder/libs/${runtime}.squashfs" "${python_dir}"
+	${ESUDO} mkdir -p "${python_dir}"
+	${ESUDO} umount "${controlfolder}/libs/${runtime}.squashfs" 2>/dev/null || true
+	${ESUDO} mount "${controlfolder}/libs/${runtime}.squashfs" "${python_dir}"
 
 	export PYTHONHOME="${python_dir}"
-	export PATH="${python_dir}/bin:$PATH"
+	export PATH="${python_dir}/bin:${PATH}"
 	export LD_LIBRARY_PATH="${python_dir}/libs:${LD_LIBRARY_PATH}"
 	python="${python_dir}/bin/python3"
 
-	$python -m pip install -U pysdl2
-	$python -m pip install -U dotenv
-	$python -m pip install -U semver
+	${python} -m pip install -U pysdl2
+	${python} -m pip install -U dotenv
+	${python} -m pip install -U semver
 }
 
 # Default to system python3
-python=$(which python3)
+python=$(command -v python3)
 
-if [ -x "$python" ]; then
-	PYTHON_VERSION=$($python -c 'import sys; print("{}.{}".format(sys.version_info[0], sys.version_info[1]))')
+if [[ -x ${python} ]]; then
+	PYTHON_VERSION=$(${python} -c 'import sys; print("{}.{}".format(sys.version_info[0], sys.version_info[1]))')
 else
 	PYTHON_VERSION="0.0"
 fi
@@ -72,16 +72,16 @@ version_ge() {
 	# returns 0 if $1 >= $2
 	v1="$1"
 	v2="$2"
-	[ "$(printf '%s\n' "$v2" "$v1" | sort -V | head -n1)" = "$v2" ]
+	[[ "$(printf '%s\n' "${v2}" "${v1}" | sort -V | head -n1)" == "${v2}" ]]
 }
 
-if ! version_ge "$PYTHON_VERSION" "$REQUIRED_VERSION"; then
+if ! version_ge "${PYTHON_VERSION}" "${REQUIRED_VERSION}"; then
 	use_runtime
 fi
 
 # Run the app
-pm_platform_helper "$python" >/dev/null
-$python main.py >"${LOG_FILE}" 2>&1
+pm_platform_helper "${python}" >/dev/null
+${python} main.py >"${LOG_FILE}" 2>&1
 
 # Cleanup
 pm_finish
